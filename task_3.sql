cddb8f68e9a3b2df755c86c2b76b65f423e0bacc
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-02 02:36:00 +0700

    adding the third task of listing all the tables in database

---
+++ task_3.sql
@@ -0,0 +1,6 @@
+-- This command is used to display a list of all non-temporary tables
+-- within the currently selected database.
+
+-- So in this I only need to pass the command of SHOW TABLES; 
+-- and in the terminal use: mysql -u root -p alx_book_store < task_3.sql
+SHOW TABLES;
