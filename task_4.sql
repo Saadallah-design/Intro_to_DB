09fded6d43821c28ea3450ef34e7b4e86d2a7918
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-02 02:36:32 +0700

    adding the 4th task for getting the description os books table

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- The standard command to get the description of tables is to use DESCRIBE Authors etc
+
+-- This script retrieves the structure of all primary tables
+-- in the alx_book_store database without using DESCRIBE or EXPLAIN.
+
+-- Get the structure of the Books table
+SHOW COLUMNS FROM Books;
+
+
+-- SHOW COLUMNS FROM Authors;
+-- SHOW COLUMNS FROM Customers;
+-- SHOW COLUMNS FROM Orders;
+-- SHOW COLUMNS FROM Order_Details;
