9525118c7251ca004223f7204c71bf83c3066db3
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-02 02:59:58 +0700

    trying new way for task 4

--- task_4.sql
+++ task_4.sql
@@ -3,11 +3,31 @@
 -- This script retrieves the structure of all primary tables
 -- in the alx_book_store database without using DESCRIBE or EXPLAIN.
 
+-- Now using Advanced Schema Query
+-- This statement works by querying MySQL's own internal metadata dictionary, which is stored in a special database called INFORMATION_SCHEMA. Think of INFORMATION_SCHEMA as the central library containing the blueprint for every database and table on your server.
+
+-- Script for books full description
+USE alx_book_store;
+
+-- Show the full description of the books table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store'
+ORDER BY ORDINAL_POSITION;
+
+
+
+
+-- =========================================
+-- This one below didn't work with the autochecker
 -- Get the structure of the Books table
-
-SHOW COLUMNS FROM books;
-
-
+-- SHOW COLUMNS FROM books;
 -- SHOW COLUMNS FROM Authors;
 -- SHOW COLUMNS FROM Customers;
 -- SHOW COLUMNS FROM Orders;
